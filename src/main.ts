c6f4b3d3ccb58e92a663fa9426f02f6cfec2edab
M.Bondarenko <m.bondarenko@upswot.com>
2025-03-08 21:14:00 +0100

    fix i18n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createI18n } from 'vue-i18n'
-
-const i18n = createI18n({
-  legacy: false,
-  locale: 'en',
-  messages: {}
-})
+import { i18n } from './i18n'
 
 const app = createApp(App)
 app.use(i18n)
